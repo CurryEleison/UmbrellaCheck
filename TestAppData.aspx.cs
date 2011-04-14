9d7328cee5a8e373d75fb15784ae70642d9fd6b8
Thomas Petersen <curryeleison@gmail.com>
2011-04-14 20:24:05 +0200

    App_Data testing

---
+++ TestAppData.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class TestAppData : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lit.Text = System.IO.File.GetLastWriteTime(Server.MapPath("~/App_Data/TextFile1.txt")).ToString(); // System.IO.File.ReadAllText(Server.MapPath("~/App_Data/TextFile.txt"));
+        System.IO.File.WriteAllText(Server.MapPath("~/App_Data/TextFile1.txt"), "slidujh lkajsd hflkadshf alkhf alk");
+    }
+}
